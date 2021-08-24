9b2b59c738d947061a84d81dbdaab7e2895ae548
viethung7899 <viethung07081999@gmail.com>
2021-08-24 02:50:39 -0700

    Clean up repository

--- src/_home/cards.tsx
+++
@@ -1,9 +0,0 @@
-import { CardProps } from './Card';
-
-export const cards: CardProps[] = [
-  {
-    title: 'Single Pendulum',
-    description: 'A simutation of the motion for a single pendulum under gravitational field',
-    link: `/01-single-pendulum`,
-  },
-];
--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './global.scss';
-import CardDisplay from './_home/CardDisplay';
-import './_home/style.scss';
-
-ReactDOM.render(<CardDisplay />, document.querySelector('.cards'));
